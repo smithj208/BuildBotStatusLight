b097ecec09d0af743b03a121282422eeeaa510f8
Andrew Milton <amilton@assurance.com>
2022-03-30 11:26:16 -0700

    Iniitial Commit
    This is the initial commit of the BuildBot Status Light Project
    
    Refer to the README document for links to BuildBot and the hardware used in this project.

---
+++ light_controller/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from .light_controller import LightController
---
+++ light_controller/ir_toy/ir_toy_settings.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from collections import namedtuple
+__author__ = 'akm'
+
+IR_ToySettings = namedtuple('IR_ToySettings', ('pr2', 'dutyCycle', 'prescaler', 'TX_Timer', 'frequency'))
